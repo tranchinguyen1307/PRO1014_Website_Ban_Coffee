674162b2f20b4c21d1b3ec9d59c34f1214f8aabb
tranchinguyen1307 <145844563+tranchinguyen1307@users.noreply.github.com>
2024-03-16 18:36:03 +0700

    sửa layout thêm product_section vao trang chủ

--- client/index.php
+++ client/index.php
@@ -1,4 +1,4 @@
 <?php include "client/particals/header.php" ?>
 <?php include "client/particals/banner.php" ?>
-<?php include "client/particals/layout_product.php"; ?>
+<?php include "client/particals/product_section.php" ?>
 <?php include "client/particals/footer.php"; ?>
